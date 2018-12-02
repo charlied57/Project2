a1f5114725510f881bd998cbf10501d39ac1e33e
Charles Davis <charlied57@gmail.com>
2018-12-01 21:39:34 -0700

    Commented and added instructions. May need tree print.

--- Helper.cpp
+++ Helper.cpp
@@ -4,8 +4,10 @@
 
 using namespace std;
 
+// Set a global variable for where the next empty spot in the tree is
 long nextEmptyIndex = 0;
 
+// Print the vector of vectors in vector form
 void printVector(vector<vector<string>> tree){
 
     for(int i = 0; i < 7; i++){
@@ -16,15 +18,21 @@ void printVector(vector<vector<string>> tree){
         }
         cout << endl;
     }
+    cout << endl;
 }
 
+// Hash function that makes the entry 8 characters long
 string hashFunction(string entry, string parentID, string ID, string lHash, string rHash){
     string null = "00000000";
+
+    // Adds random char to make the entry 8 characters
     if(entry.length() < 8){
         for(int i = entry.length(); i < 8; i++) {
             entry.insert(0, 1, static_cast<char>(rand() % 95 + 32));
         }
     }
+
+    // Sums every 8th character and then converts that sum to a character
     vector<long>entrySumVector (8, 0);
     for(short i = 0; i < entry.length(); i++){
         entrySumVector[i % 8] = entrySumVector[i % 8] + ((long)(entry.at(i))) * i;
@@ -36,9 +44,11 @@ string hashFunction(string entry, string parentID, string ID, string lHash, stri
 
     vector<string>temp;
 
+    // Pushback elements that are always present
     temp.push_back(tempString);
     temp.push_back(parentID);
 
+    // Pushback if element is persent
     if(ID != null){
         temp.push_back(ID);
     }
@@ -49,6 +59,7 @@ string hashFunction(string entry, string parentID, string ID, string lHash, stri
         temp.push_back(rHash);
     }
 
+    // Hash the strings
     string hashValue;
     for(int i = 0; i < 8; i++){
         long charSum = 0;
@@ -58,11 +69,15 @@ string hashFunction(string entry, string parentID, string ID, string lHash, stri
         hashValue.insert(0, 1, static_cast<char>(rand() % 95 + 32));
     }
 
+    // Return hash
     return hashValue;
 }
 
+// Recursive function that updates all the parents in the tree
 vector<vector<string>> updateParent(vector<vector<string>> tree, long parentIndex){
     string null = "00000000";
+
+    // Decide if the the node is left or right
     bool left;
     if((parentIndex-1) % 2 == 0){
         left = true;
@@ -71,8 +86,12 @@ vector<vector<string>> updateParent(vector<vector<string>> tree, long parentInde
         left = false;
     }
 
+    // Update the left hash and history
     if(left){
+        // Update left hash
         tree[(parentIndex - 1)/2][3] = hashFunction(tree[nextEmptyIndex][2], tree[nextEmptyIndex][1], tree[nextEmptyIndex][0], tree[nextEmptyIndex][3], tree[nextEmptyIndex][4]);
+
+        // If history isn't null then append hash to history else set history equal hash
         if(tree[(parentIndex - 1)/2][5] == null){
             tree[(parentIndex - 1)/2][5] = tree[(parentIndex - 1)/2][3];
         }
@@ -80,8 +99,12 @@ vector<vector<string>> updateParent(vector<vector<string>> tree, long parentInde
             tree[(parentIndex - 1)/2][5].append(tree[(parentIndex - 1)/2][3]);
         }
     }
+    // Update the right hash and history
     else{
+        // Update the right hash
         tree[(parentIndex - 1)/2][4] = hashFunction(tree[nextEmptyIndex][2], tree[nextEmptyIndex][1], tree[nextEmptyIndex][0], tree[nextEmptyIndex][3], tree[nextEmptyIndex][4]);
+
+        // If history isn't null then append hash to history else set history equal hash
         if(tree[(parentIndex - 1)/2][6] == null){
             tree[(parentIndex - 1)/2][6] = tree[(parentIndex - 1)/2][4];
         }
@@ -90,9 +113,11 @@ vector<vector<string>> updateParent(vector<vector<string>> tree, long parentInde
         }
     }
 
+    // If parent is root then return
     if((parentIndex - 1)/2 == 0) {
         return tree;
     }
+    // else recursively call update parent with the parent index
     else{
         tree = updateParent(tree, (parentIndex-1)/2);
 
@@ -100,6 +125,7 @@ vector<vector<string>> updateParent(vector<vector<string>> tree, long parentInde
     return tree;
 }
 
+// Create a new level of empty nodes
 vector<vector<string>> createNewLevel(vector<vector<string>> tree, string null){
 
     long currentSize = tree.size();
@@ -117,6 +143,7 @@ vector<vector<string>> createNewLevel(vector<vector<string>> tree, string null){
     return tree;
 }
 
+// Creates a random string of 8 characters
 string randNum(){
     string randNum;
     for(int i = 0; i < 8; i++){
@@ -125,10 +152,12 @@ string randNum(){
     return randNum;
 }
 
+// Update the next node with the new entry
 vector<vector<string>> addEntry(vector<vector<string>> tree, string entry){
 
     string null = "00000000";
 
+    // If creating roor then run this statement
     if(tree.empty()){
         vector<string>temp;
         string parentID = randNum();
@@ -142,6 +171,7 @@ vector<vector<string>> addEntry(vector<vector<string>> tree, string entry){
         tree.push_back(temp);
         nextEmptyIndex++;
     }
+    // If the tree is full create a new row then add new information and update the tree
     else if(nextEmptyIndex == tree.size()){
         tree = createNewLevel(tree, null);
         vector<string>temp;
@@ -156,6 +186,7 @@ vector<vector<string>> addEntry(vector<vector<string>> tree, string entry){
         tree = updateParent(tree, nextEmptyIndex);
         nextEmptyIndex++;
     }
+    // Add new information then update the tree
     else{
         vector<string>temp;
         temp.push_back(hashFunction(entry, tree[(nextEmptyIndex-1)/2][1], null, null, null));
@@ -170,6 +201,5 @@ vector<vector<string>> addEntry(vector<vector<string>> tree, string entry){
         nextEmptyIndex++;
     }
 
-    printVector(tree);
     return tree;
 }
--- main.cpp
+++ main.cpp
@@ -9,27 +9,43 @@ int main() {
     vector<vector<string>> tree;
 
     string null = "00000000";
+    string instructions = "Input string less than 1024 characters, print-tree, print-vector, help, quit.";
 
     string entry;
 
+    cout << instructions << endl;
+
+    // Loop inputs
     while(true){
 
+        // Loop if more than 1024 characters
         do{
             cout << "Input: ";
             cin >> entry;
+            cout << endl;
         }while(entry.size() > 1024);
 
+        // Quit the program
         if(entry == "quit") {
             return 0;
         }
 
-        if(entry == "print"){
+        // Print in tree form(not working currently)
+        else if(entry == "print-tree"){
 //            printTree(tree);
         }
-        if(entry == "printV"){
+
+        // Print in vector form(kinda messy)
+        else if(entry == "print-vector"){
             printVector(tree);
         }
 
+        // Print instructions
+        else if(entry == "help"){
+            cout << endl << instructions << endl << endl;
+        }
+
+        // Add next entry and begin the main program
         else{
             tree = addEntry(tree, entry);
         }
